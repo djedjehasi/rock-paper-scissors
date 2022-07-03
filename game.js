829de8a8fb8fbb3f5fd05405a1f32158679a1f5a
Yosef Gehasy <djedjehasi@gmail.com>
2022-07-03 16:09:05 +0300

    fix the code

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ let palyerSelection;
 let computerSelection;
 let playerWins = 0;
 let computerWins = 0;  
-const shapes = ["rock", "paper", "sicssors"]
+const shapes = ["rock", "paper", "scissors"]
 // function declartions:
 
 // computer selction function
@@ -17,13 +17,13 @@ function computerPlay() {
             hand = shapes[0];
             break;
         case 1:
-            hand =shapes[1];
+            hand = shapes[1];
             break;
         case 2:
             hand = shapes[2];
             break;
     }
-    return hand;
+    return computerSelection = hand;
 }
 
  
@@ -32,27 +32,28 @@ function playRound(palyerSelection, computerSelection){
     if (palyerSelection.toLowerCase() === computerPlay) {
         return "tie"
     }else if(palyerSelection.toLowerCase() === shapes[0]){
-        if (computerPlay == shapes[1]){
+        if (computerSelection === shapes[1]){
+            computerWins++;
             return "Paper is waeker then rock. You lose!"
-            computerWins++;
         }else{
-            return "Rock is stronger then scissors. You win!"
-        }       playerWins++;
-    }else if(palyerSelection.toLowerCase() === shapes[1]){
-        if (computerPlay === shapes[0]){
-            return "Paper is stronger then rock. You win!"
             playerWins++;
+            return "Rock is stronger then scissors. You win!"
+        }     
+    }else if(palyerSelection.toLowerCase() === shapes[1]){
+        if (computerSelection === shapes[0]){
+            playerWins++;
+            return "Paper is stronger then rock. You win!"
         }else{
-            return "Scissors is stronger then paper. You lose!"
             computerWins++;
+            return "Scissors is stronger then paper. You lose!"
         }
     }else if (palyerSelection.toLowerCase() === shapes[2]){
-        if(computerPlay === shapes[0]){
-            return "Rock is stronger then scissors. You Lose"
+        if(computerSelection === shapes[0]){
             computerWins++;
+            return "Rock is stronger then scissors. You lose!"            
         } else{
-            return "Scissors is stronger then paper. You win!"
             playerWins++;
+            return "Scissors is stronger then paper. You win!"
         }
     }
 
@@ -61,15 +62,16 @@ function playRound(palyerSelection, computerSelection){
 // game function: 5 runs of play round. 
     function game (){
         for(let i = 0; i < 5; i++){                                         // start of loop;
-            palyerChoise = prompt("rock, paper or scissors?");              // askinf for value;
+            let playerChoise = prompt("rock, paper or scissors?");              // askinf for value;
             computerChoice = computerPlay();                                // initilazing computer value;
-            playRound(palyerChoise, computerChoice);                        // run round of the game;
-            console.log(playRound(palyerChoise, computerChoice));           // print statment of win, lose or tie;
+            playRound(playerChoise, computerChoice);                        // run round of the game;
+            console.log(playRound(playerChoise, computerChoice));           // print statment of win, lose or tie;
         }                                                                   // end of the loop; 
-        if (palayerWins > computerWins){                                    // test who wins and print winner statment;
-            console.log("congratulations! you won!")
-        } else {
-            console.log("to bad, you lose..")
+        if(playerWins > computerWins){
+            console.log("congrts! You'er the WINER!!");
+        }
+        else{
+            console.log("Too bad. you'er LOSER!!");
         }
     }
 // invoke the game function:
